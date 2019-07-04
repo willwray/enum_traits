000f9d11d23ffa69ce99d82fb75c269c3a5586a0
Will Wray <wjwray@gmail.com>
2019-07-04 14:03:20 -0400

    add noexcept on to_underlying signature

--- enum_traits.hpp
+++ enum_traits.hpp
@@ -76,7 +76,7 @@ struct underlying_type : impl::underlying_type<T> {};
 // to_underlying(e): convenience cast from enum value e to its underlying type
 //
 template <typename E>
-constexpr auto to_underlying(E e)
+constexpr auto to_underlying(E e) noexcept
 {
     return static_cast<std::underlying_type_t<E>>(e);
 }
